2ef79bc8b20849f24e1d0dd72c07b3a68d7115b5
Dhanshri Rc <dhanshrirc@gmail.com>
2026-07-10 14:42:23 +0530

    Refactor navigation: remove unused routes and clean up Navbar items

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,10 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Authors from "./pages/Authors";
 import Journals from "./pages/Journals";
-import Reviewers from "./pages/Reviewers";
-import Resources from "./pages/Resources";
 import EditorialBoard from "./pages/EditorialBoard";
 import AuthorGuidlines from "./pages/AuthorGuidelines";
 import CurrentIssue from "./pages/CurrentIssue";
 import Archives from "./pages/Archives";
-import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 import ScrollToTop from "./components/ScrollToTop";
 
@@ -28,13 +25,12 @@ const AnimatedRoutes = () => {
         <Route path="/about" element={<About />} />
         <Route path="/authors" element={<Authors />} />
         <Route path="/journals" element={<Journals />} />
-        <Route path="/reviewers" element={<Reviewers />} />
-        <Route path="/resources" element={<Resources />} />
+  
         <Route path="/editorial-board" element={<EditorialBoard />} />
         <Route path="/author-guidelines" element={<AuthorGuidlines />} />
         <Route path="/current-issue" element={<CurrentIssue />} />
         <Route path="/archives" element={<Archives />} />
-        <Route path="/services" element={<Services />} />
+      
         <Route path="/contact" element={<Contact />} />
         {/* Fallback route - redirect unknown paths to Home content (SPA friendly) */}
         <Route path="*" element={<Home />} />
